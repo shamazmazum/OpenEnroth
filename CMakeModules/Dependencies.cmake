8b7803c6d5d4b5be21f1c609076c145804ca32f7
Vasily Postnicov <shamaz.mazum@gmail.com>
2023-11-11 09:00:07 +0300

    Use LuaJIT from system

--- CMakeModules/Dependencies.cmake
+++ CMakeModules/Dependencies.cmake
@@ -83,5 +83,7 @@ macro(resolve_dependencies) # Intentionally a macro - we want set() to work in p
         find_package(ZLIB REQUIRED)
         find_package(OpenAL REQUIRED)
         find_package(SDL2 REQUIRED)
+        find_package(PkgConfig REQUIRED)
+        pkg_check_modules(LuaJit REQUIRED luajit)
     endif()
 endmacro()
